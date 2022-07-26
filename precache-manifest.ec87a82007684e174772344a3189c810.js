27adbd4973bf878e5718cd42a960f0e7b0f9d06c
Vijaykannnan <vijaykannan805@gmail.com>
2022-07-26 10:01:37 +0530

    Updates

---
+++ precache-manifest.ec87a82007684e174772344a3189c810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4d06a2cb1d202066099",
+    "url": "/landing-page-via-react/static/css/main.05d9a0c1.chunk.css"
+  },
+  {
+    "revision": "d4d06a2cb1d202066099",
+    "url": "/landing-page-via-react/static/js/main.d4d06a2c.chunk.js"
+  },
+  {
+    "revision": "47e450cba40c46bb1046",
+    "url": "/landing-page-via-react/static/js/1.47e450cb.chunk.js"
+  },
+  {
+    "revision": "6703221348da3da32153",
+    "url": "/landing-page-via-react/static/js/runtime~main.67032213.js"
+  },
+  {
+    "revision": "b1ae80bcddbc7c414e8dc15ae7281dcf",
+    "url": "/landing-page-via-react/static/media/cs.b1ae80bc.png"
+  },
+  {
+    "revision": "ba31c9ee2032adb0e0861bd97fba4dca",
+    "url": "/landing-page-via-react/index.html"
+  }
+];
